bf653bd21714193473e911267adc93cfbb9fbb60
Timothy <timauthx@gmail.com>
2021-02-15 01:14:34 +0200

    feat (app) asset form WIP

--- iotchain/lib/views/asset_form.dart
+++ iotchain/lib/views/asset_form.dart
@@ -1,10 +1,9 @@
-import 'dart:io';
-
-import 'package:dart_json_mapper/dart_json_mapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:dart_json_mapper/dart_json_mapper.dart';
+import 'package:iotchain/controllers/blockchain_adapter.dart';
 import 'package:iotchain/model/organization_model.dart';
-
-import '../model/asset_model.dart';
+import 'package:iotchain/model/asset_model.dart';
 
 class AssetForm extends StatefulWidget {
   @override
@@ -12,12 +11,33 @@ class AssetForm extends StatefulWidget {
 }
 
 class _AssetFormState extends State<AssetForm> {
-  Asset asset;
-  final List<Organization> organizations = JsonMapper.deserialize(File("assets/data/organizations.json").readAsStringSync());
-  final List<AssetType> assetTypes = JsonMapper.deserialize(File("assets/data/asset_types.json").readAsStringSync());
+  Asset asset = Asset();
+  List<Organization> organizations = <Organization>[];
+  List<AssetType> assetTypes = <AssetType>[];
+
   final _formKey = GlobalKey<FormState>();
 
-  Future<void> submitAsset() async {}
+  Future<void> submitAsset() async {
+    if (_formKey.currentState.validate()) {
+      try {
+        var jsonData = JsonMapper.serialize(asset);
+        await Blockchain.submitTransaction("assets", "Insert", jsonData);
+        Navigator.pop(context);
+      } on Exception catch (e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.toString())));
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    rootBundle.loadString("assets/data/organizations.json").then((value) =>
+        setState(() => organizations = JsonMapper.deserialize<List<Organization>>(value)));
+    rootBundle.loadString("assets/data/asset_types.json").then((value) =>
+        setState(() => assetTypes = JsonMapper.deserialize<List<AssetType>>(value)));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +65,12 @@ class _AssetFormState extends State<AssetForm> {
                             hintText: "Enter an asset name",
                             labelText: "Name",
                           ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Please provide name for new asset";
+                            }
+                            return null;
+                          },
                           onChanged: (value) {
                             setState(() => asset.name = value);
                           },
@@ -55,8 +81,14 @@ class _AssetFormState extends State<AssetForm> {
                             hintText: "Enter an SKU code",
                             labelText: "SKU code",
                           ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Please provide SKU code for new asset";
+                            }
+                            return null;
+                          },
                           onChanged: (value) {
-                            setState(() => asset.name = value);
+                            setState(() => asset.sku = value);
                           },
                         ),
                         DropdownButtonFormField(
@@ -64,24 +96,46 @@ class _AssetFormState extends State<AssetForm> {
                             filled: true,
                             hintText: "Choose asset type",
                             labelText: "Asset type",
-                            fillColor: Colors.teal.shade600,
                           ),
-                          items: assetTypes.map<DropdownMenuItem<String>>(
+                          items: assetTypes
+                              .map<DropdownMenuItem<String>>(
                                   (org) => DropdownMenuItem<String>(
-                                value: org.type,
-                                child: Text(org.name),
-                              ))
+                                        value: org.type,
+                                        child: Text(org.name),
+                                      ))
                               .toList(),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please choose an asset type";
+                            }
+                            return null;
+                          },
                           onChanged: (value) {
-                            setState(() => asset.owner = value);
+                            setState(() => asset.type = value);
                           },
                         ),
                         TextFormField(
                           decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
                             filled: true,
+                            hintText: "Enter an cost",
+                            labelText: "Cost",
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Please provide cost for new asset";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            setState(() => asset.cost = double.parse(value));
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
                             hintText: "Enter asset description",
                             labelText: "Description",
+                            filled: true,
                           ),
                           onChanged: (value) {
                             setState(() => asset.info = value);
@@ -90,31 +144,38 @@ class _AssetFormState extends State<AssetForm> {
                         ),
                         DropdownButtonFormField(
                           decoration: InputDecoration(
-                            filled: true,
                             hintText: "Choose the owner organization",
                             labelText: "Owned by",
-                            fillColor: Colors.teal.shade600,
+                            filled: true,
                           ),
-                          items: organizations.map<DropdownMenuItem<String>>(
+                          items: organizations
+                              .map<DropdownMenuItem<String>>(
                                   (org) => DropdownMenuItem<String>(
-                                value: org.mspID,
-                                child: Text(org.name),
-                              ))
+                                        value: org.mspID,
+                                        child: Text(org.name),
+                                      ))
                               .toList(),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please choose an owner organization";
+                            }
+                            return null;
+                          },
                           onChanged: (value) {
                             setState(() => asset.owner = value);
                           },
                         ),
-                        ElevatedButton(
+                        SizedBox(
+                            width: double.infinity,
+                            height: 45,
+                            child: ElevatedButton(
                           onPressed: submitAsset,
                           child: const Text("Submit asset",
                               style: TextStyle(fontSize: 20)),
-                        ),
+                        )),
                       ].expand((widget) => [
                             widget,
-                            SizedBox(
-                              height: 24,
-                            )
+                            SizedBox(height: 24,)
                           ])
                     ]),
               ),
