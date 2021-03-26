982c7fd85a27b6ff86a8efed276c50f446c17635
Timothy <timauthx@gmail.com>
2021-03-27 01:53:01 +0200

    feat: add metrics icons

--- app/lib/views/components/svg_icon.dart
+++ app/lib/views/components/svg_icon.dart
@@ -6,16 +6,18 @@ class SvgIcon extends StatelessWidget {
   final Color color;
   final double size;
 
-  SvgIcon(
+  const SvgIcon(
     this.icon, {
     this.color,
     this.size = 24,
   });
 
   @override
-  Widget build(BuildContext context) => SvgPicture.asset(
-    "assets/icons/$icon.svg",
-    color: color,
-    width: size,
-  );
+  Widget build(BuildContext context) => icon != null
+      ? SvgPicture.asset(
+          "assets/icons/$icon.svg",
+          color: color ?? Theme.of(context).iconTheme.color,
+          width: size,
+        )
+      : Icon(Icons.help);
 }
